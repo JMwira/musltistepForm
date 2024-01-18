27472f04b9993d8a9248cd016fb01f11d6b9e82f
JMwira <jaymwira@gmail.com>
2024-01-18 11:54:11 +0200

    thanks message

---
+++ src/steps/Five.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Five = () => {
+  return (
+    <div className='flex flex-col text-center self-center items-center justify-center' >
+      <span className='text-3xl font-extrabold' >Thank you</span>
+      <span className='lg:w-[80%] font-bold mt-5 text-slate-400'>Thaks for confirming your subscription! We hope youhave fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com</span>
+    </div>
+  )
+}
+
+export default Five
